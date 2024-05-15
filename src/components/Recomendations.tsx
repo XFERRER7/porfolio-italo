c5b8954eb0c8fb9d867e92ff50f12ab2b72a429f
XFERRER7 <alexandretalles777@gmail.com>
2024-05-15 12:08:41 -0300

    reccomendations card with real data

--- src/components/Recomendations.tsx
+++ src/components/Recomendations.tsx
@@ -6,19 +6,18 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { useLanguage } from '@/hooks/useLanguage'
+import Image from 'next/image';
 
 export const Recomendations = () => {
+  const { translations } = useLanguage();
+
   return (
     <div className='w-full lg:h-80 bg-primary-100 rounded-xl border-2 border-quaternary-100 py-8 px-14'>
-
-
-      <h3 className='font-semibold text-lg'>Recomendations</h3>
-      <p className='text-quinternary-100 text-sm mt-1'>"People who trust in my work."</p>
-
+      <h3 className='font-semibold text-lg'>{translations.recommendations.title}</h3>
+      <p className='text-quinternary-100 text-sm mt-1'>{translations.recommendations.subtitle}</p>
 
       <div className='flex mt-5'>
-
-
         <Carousel
           opts={{
             align: "start",
@@ -26,58 +25,56 @@ export const Recomendations = () => {
           className="w-full"
         >
           <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index} className="sm:basis-1/2 lg:basis-1/2 xl:basis-1/3 flex items-center justify-center">
+            {translations.recommendations.cards.map((item, index) => (
+              <CarouselItem key={`${index}-${item.name}`} className="sm:basis-1/2 lg:basis-1/2 xl:basis-1/3 flex items-center justify-center">
                 <div className="p-1">
-                  <Card />
+                  <Card
+                    name={item.name}
+                    position={item.position}
+                    quote={item.quote}
+                    image={item.image}
+                  />
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious
-            className='bg-primary-100 border-quinternary-100'
-          />
-          <CarouselNext
-            className='bg-primary-100 border-quinternary-100'
-          />
+          <CarouselPrevious className='bg-primary-100 border-quinternary-100  active:bg-primary-100 focus:bg-primary-100' />
+          <CarouselNext className='bg-primary-100 border-quinternary-100 active:bg-primary-100 focus:bg-primary-100' />
         </Carousel>
-
       </div>
-
     </div>
   )
 }
 
+interface ICardProps {
+  name: string;
+  position: string;
+  quote: string;
+  image: string;
+}
 
-const Card = () => {
+const Card = ({ name, position, quote, image }: ICardProps) => {
 
   return (
     <div className='gradient-border p-[1.5px] rounded-3xl recomendation-card-border'>
-      <div className='h-48 lg:w-[20rem] w-full bg-tertiary-100 rounded-3xl text-white flex flex-col gap-5 justify-center px-5 py-5'>
-
+      <div className='h-48 lg:w-[20rem] w-full bg-tertiary-100 rounded-3xl text-white flex flex-col gap-5 justify-center px-5 py-5 overflow-hidden'>
         <div className='flex items-center gap-3'>
-          {/* <Image 
-          src='/images/face.jpg'
-          alt='face'
-          width={100}
-          height={100}
-          className='rounded-full'
-        /> */}
-
-          <div className='h-12 w-12 rounded-full bg-quinternary-100' />
+          
+          <Image
+            src={image}
+            alt='face'
+            width={100}
+            height={100}
+            className='rounded-full w-14 h-14'
+          />
 
           <div>
-            <h4>John Doe</h4>
-            <p className='text-[10px]'>CEO at Company</p>
+            <h4>{name}</h4>
+            <p className='text-[10px]'>{position}</p>
           </div>
         </div>
-
-        <p className='text-sm'>
-          s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-        </p>
-
+        <p className='text-sm overflow-hidden text-ellipsis lg:line-clamp-4 line-clamp-3 cursor-pointer' title={quote}>{quote}</p>
       </div>
     </div>
   )
-
 }
--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -75,6 +75,60 @@ export const pt = {
     title: "Kiwi Manga: Manga Manhwa",
     description: "Meu primeiro aplicativo atingiu mais de 50.000 downloads!"
   },
+  recommendations: {
+    title: "Recomendações",
+    subtitle: "Pessoas que confiam no meu trabalho.",
+    cards: [
+      {
+        name: "Giseli Gouveia",
+        position: "UX Designer",
+        quote: "Tive a grata oportunidade de trabalhar com o Italo por quase um ano, num mesmo projeto, e ele sempre se mostrou pró-ativo, colaborativo, comunicador, preocupado em atender as demandas e em prover as melhores soluções para os usuários e cliente. Uma pessoa excelente para trabalhar em conjunto e com muita vontade de evoluir em sua carreira.",
+        image: "https://media.licdn.com/dms/image/C4E03AQHYHvFmyLJ89w/profile-displayphoto-shrink_400_400/0/1516334112500?e=1721260800&v=beta&t=2JMjV_pcDfV4KPtzxmpSdd_P9peYcUP3xpIXxGryRSI"
+      },
+      {
+        name: "Amanda Luiza",
+        position: "Desenvolvedora Web",
+        quote: "Tive a honra de trabalhar com Italo e ele simplesmente me mostrou ser um excelente profissional tanto na parte técnica quanto na parte da comunicação. Muito dedicado, talentoso e com foco em resolução de problemas.",
+        image: "https://media.licdn.com/dms/image/D4D03AQHj2-61qKAhHg/profile-displayphoto-shrink_100_100/0/1673126204609?e=1721260800&v=beta&t=I4jDNPXJvdfrFT4em8PvRSztUUO6Nw2NfuHtoYAgUaQ"
+      },
+      {
+        name: "Josemar Carvalho",
+        position: "Desenvolvedor",
+        quote: "Profissional muito dedicado e compromissado com as tarefas, mente aberta ao predizendo, em todo tempo focado nas entrega e melhoria do produto. Foi uma satisfação trabalhar com o Ítalo no projeto flutter da intelbras onde desenvolvemos juntos um app android/iOS para o produto IH3001. O IH3001 foi um projeto que podemos aplicar alguma técnicas de desenvolvimento o desenvolvemos voltada para aplicações embarcada no roteador IH3001 da Intelbras, eu poderia falar que o maior desfaio entre todos foi implementara uma chamada multicast no iOS, tivemos alguns embates com a Apple nessas questões más no final deu tudo certo, o app IH3001 consumi-o alguns recursos como, manipulação do roteador via HTTP Rest API, geração e compartilhamento de imagem Qrcode, Leitura de Qrcode, chamada multicast UDP para fazer Discovery na rede computacional, Aplicação do conceito de micro-apps, criar navegação personalizadas e animadas, gerar e consumir temas customizado para aplicação, bloc como gerenciador de estados, criação de widgets reativa, entre outros desafios, eu recomendo esse profissional com muita satisfação.",
+        image: "https://media.licdn.com/dms/image/C4E03AQEkmKFRNrWt1Q/profile-displayphoto-shrink_400_400/0/1637503807692?e=1721260800&v=beta&t=hyNi7IXDVM3wHmf7JB4KELKeZau8E4DQRftyqfAqSQE"
+      },
+      {
+        name: "Daniele Renata Silva",
+        position: "Project Manager",
+        quote: "O Ítalo é extremamente proativo, é autodidata, não vê dificuldade que não possa ser superada, é bastante positivo. Uma pessoa com excelente entrega técnica e excelente comunicação. Muito bom em se relacionar com outras pessoas e se saiu muito bem num contexto com uma equipe grande com entregas constantes.",
+        image: "https://media.licdn.com/dms/image/C4D03AQHe3bQv395Z3w/profile-displayphoto-shrink_100_100/0/1646260823041?e=1721260800&v=beta&t=p3jlBPGSbXRH1M79vGJT3p_9juq8vKhYrtol8Th8kJg"
+      },
+      {
+        name: "Erluan Felix Batista",
+        position: "Tech Lead",
+        quote: "O Italo é um dos profissionais mais dedicados que já pude ter o prazer de colaborar em conjunto. Tem uma visão de como podemos melhorar tal aplicação, desde o âmbito técnico até na valorização do negócio, e tem senso de urgência. É imprescindível reforçar que o Ítalo também tem sede de aprender. Está sempre disposto a ouvir, a compartilhar e a buscar conhecimento.",
+        image: "https://media.licdn.com/dms/image/C5103AQGQMGbCMpYr8Q/profile-displayphoto-shrink_100_100/0/1516928738084?e=1721260800&v=beta&t=2LgXxMAofKl60MtG51xGasyLRMuZiBRiFCj6xDYmNvg"
+      },
+      {
+        name: "Gabriela Bastos",
+        position: "Gestão de Projetos",
+        quote: "Ótima comunicação e proatividade durante o desenvolvimento do projeto!",
+        image: "https://media.licdn.com/dms/image/D4D03AQFfuvbQ_1u5Lg/profile-displayphoto-shrink_100_100/0/1713202567320?e=1721260800&v=beta&t=1QPZdDCMTyzYATFok8qXStaN_KMUyxtKSiyfkPvvzuc"
+      },
+      {
+        name: "Luan Albrechetti",
+        position: "Software Engineer",
+        quote: "Gostei muito de trabalhar junto com o Italo, ele é muito esforçado e aprende rápido. Tem um bom domínio referente ao Flutter e sempre esta buscando aprimorar seus conhecimento!",
+        image: "https://media.licdn.com/dms/image/C4E03AQGdaDdXl9-PRw/profile-displayphoto-shrink_100_100/0/1641145881104?e=1721260800&v=beta&t=vl55LcmTeGmsTYMFujZsldkjekvVhubx0TGm5NGEQV8"
+      },
+      {
+        name: "Mario Candido",
+        position: "Software Engineer",
+        quote: "Me identifiquei com o Italo e seu irmão gêmeo desde o primeiro dia de aula no nosso curso de ADS. Italo corre atras para resolver os problemas e sempre esta disposto a ajudar os colegas com tarefas e ensinar como as coisas funcionam. Pró-ativo e inteligente. Adoraria um dia trabalhar em algum projeto com ele fora da faculdade!",
+        image: "https://media.licdn.com/dms/image/D4D03AQEn3namQGv55g/profile-displayphoto-shrink_100_100/0/1713634073833?e=1721260800&v=beta&t=4ggnSfjDEUB_DD5XgcjCkn8r7KrVOhbIxFr9rDzcLzo"
+      },
+    ]
+  },
   footer: {
     availableForJob: "DISPONÍVEL PARA TRABALHO",
   }
@@ -155,7 +209,63 @@ export const en = {
     title: "Kiwi Manga: Manga Manhwa",
     description: "My first app reached a margin of over 50,000 downloads!"
   },
+  recommendations: {
+    title: "Recommendations",
+    subtitle: "People who trust in my work.",
+    cards: [
+      {
+        name: "Giseli Gouveia",
+        position: "UX Designer",
+        quote: "I had the pleasant opportunity to work with Italo for almost a year on the same project, and he always showed himself to be proactive, collaborative, communicative, concerned with meeting demands and providing the best solutions for users and clients. An excellent person to work with and very eager to progress in his career.",
+        image: "https://media.licdn.com/dms/image/C4E03AQHYHvFmyLJ89w/profile-displayphoto-shrink_400_400/0/1516334112500?e=1721260800&v=beta&t=2JMjV_pcDfV4KPtzxmpSdd_P9peYcUP3xpIXxGryRSI"
+      },
+      {
+        name: "Amanda Luiza",
+        position: "Web Developer",
+        quote: "I had the honor of working with Italo and he simply proved to be an excellent professional both in technical and communication aspects. Very dedicated, talented, and focused on problem-solving.",
+        image: "https://media.licdn.com/dms/image/D4D03AQHj2-61qKAhHg/profile-displayphoto-shrink_100_100/0/1673126204609?e=1721260800&v=beta&t=I4jDNPXJvdfrFT4em8PvRSztUUO6Nw2NfuHtoYAgUaQ"
+      },
+      {
+        name: "Josemar Carvalho",
+        position: "Developer",
+        quote: "A very dedicated and committed professional, open-minded to improvements, always focused on delivery and product improvement. It was a pleasure to work with Italo on the Intelbras flutter project where we developed an Android/iOS app for the IH3001 product. The IH3001 was a project where we applied some development techniques aimed at applications embedded in the Intelbras IH3001 router. I could say that the biggest challenge was implementing a multicast call on iOS. We had some issues with Apple on this matter, but in the end, everything worked out. The IH3001 app used some resources such as manipulating the router via HTTP Rest API, generating and sharing QR code images, QR code reading, UDP multicast call for network discovery, micro-apps concept application, creating personalized and animated navigation, generating and consuming custom themes for the application, bloc as a state manager, creating reactive widgets, among other challenges. I highly recommend this professional.",
+        image: "https://media.licdn.com/dms/image/C4E03AQEkmKFRNrWt1Q/profile-displayphoto-shrink_400_400/0/1637503807692?e=1721260800&v=beta&t=hyNi7IXDVM3wHmf7JB4KELKeZau8E4DQRftyqfAqSQE"
+      },
+      {
+        name: "Daniele Renata Silva",
+        position: "Project Manager",
+        quote: "Italo is extremely proactive, self-taught, doesn't see any difficulty that cannot be overcome, and is very positive. A person with excellent technical delivery and excellent communication. Very good at relating to other people and performed very well in a context with a large team with constant deliveries.",
+        image: "https://media.licdn.com/dms/image/C4D03AQHe3bQv395Z3w/profile-displayphoto-shrink_100_100/0/1646260823041?e=1721260800&v=beta&t=p3jlBPGSbXRH1M79vGJT3p_9juq8vKhYrtol8Th8kJg"
+      },
+      {
+        name: "Erluan Felix Batista",
+        position: "Tech Lead",
+        quote: "Italo is one of the most dedicated professionals I have had the pleasure to collaborate with. He has a vision of how we can improve an application, from the technical scope to business enhancement, and has a sense of urgency. It is essential to emphasize that Italo also has a thirst for learning. He is always willing to listen, share, and seek knowledge.",
+        image: "https://media.licdn.com/dms/image/C5103AQGQMGbCMpYr8Q/profile-displayphoto-shrink_100_100/0/1516928738084?e=1721260800&v=beta&t=2LgXxMAofKl60MtG51xGasyLRMuZiBRiFCj6xDYmNvg"
+      },
+      {
+        name: "Gabriela Bastos",
+        position: "Project Management",
+        quote: "Great communication and proactivity during the project development!",
+        image: "https://media.licdn.com/dms/image/D4D03AQFfuvbQ_1u5Lg/profile-displayphoto-shrink_100_100/0/1713202567320?e=1721260800&v=beta&t=1QPZdDCMTyzYATFok8qXStaN_KMUyxtKSiyfkPvvzuc"
+      },
+      {
+        name: "Luan Albrechetti",
+        position: "Software Engineer",
+        quote: "I really enjoyed working with Italo, he is very hardworking and learns quickly. He has a good command of Flutter and is always looking to improve his knowledge!",
+        image: "https://media.licdn.com/dms/image/C4E03AQGdaDdXl9-PRw/profile-displayphoto-shrink_100_100/0/1641145881104?e=1721260800&v=beta&t=vl55LcmTeGmsTYMFujZsldkjekvVhubx0TGm5NGEQV8"
+      },
+      {
+        name: "Mario Candido",
+        position: "Software Engineer",
+        quote: "I identified with Italo and his twin brother from the first day of class in our ADS course. Italo is proactive in solving problems and is always willing to help colleagues with tasks and teach how things work. Proactive and intelligent. I would love to work on a project with him outside of college one day!",
+        image: "https://media.licdn.com/dms/image/D4D03AQEn3namQGv55g/profile-displayphoto-shrink_100_100/0/1713634073833?e=1721260800&v=beta&t=4ggnSfjDEUB_DD5XgcjCkn8r7KrVOhbIxFr9rDzcLzo"
+      }
+    ]
+  },
   footer: {
     availableForJob: "AVAILABLE FOR JOB"
   }
 }
+
+export type ILanguage = typeof pt
